3cf142c4924aac9c4b031e0ed06b07aa74be333b
Bayhackplis <123596328+bayhackplis1@users.noreply.github.com>
2024-10-31 15:03:28 -0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,7 @@
+const settings = {
+  ownerName: 'NamaOwnerLu', // Ganti dengan nama owner lu
+  botToken: '7212279495:AAH7BSHeCVzhinQJSpFvPPr4fjMrv25-mPg', // Ganti dengan token bot Telegram lu
+  owner: '7076123485', //OWNER user id
+};
+
+module.exports = settings;
