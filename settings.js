31185f4b4d347c759a40dea68d09277ada12c20b
Mistikjjjs <kennvortex123@gmail.com>
2024-11-01 10:29:56 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
   ownerName: 'NamaOwnerLu', // Ganti dengan nama owner lu
-  botToken: '7212279495:AAH7BSHeCVzhinQJSpFvPPr4fjMrv25-mPg', // Ganti dengan token bot Telegram lu
-  owner: '7076123485', //OWNER user id
+  botToken: '8117993552:AAGPYA_j84JiOFGJ2lqM5_lqigjIwQmZnvs', // Ganti dengan token bot Telegram lu
+  owner: '7051627600', //OWNER user id
 };
 
 module.exports = settings;
